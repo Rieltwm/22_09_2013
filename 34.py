3b847ed8f8078efd4b42cc2c3887287dfbe1a1ac
Владимир <9646310@gmail.com>
2023-09-13 16:06:13 +0300

    Для проверки задача 34

--- 34.py
+++ 34.py
@@ -19,8 +19,8 @@ def word(words):
     f = lambda x: sum(1 for i in x if i in 'аоуыэеёиюя')
     tmp = f(str[0])
     if all([f(i) == tmp for i in str]):
-        return 'Парам пам-пам'
-    return 'Пам парам'
+        return 'Ппарам пам-пам'
+    return 'Ппам парам'
 
 
 print(word("пара-ра-рам рам-пам-папам па-ра-па-да "))
